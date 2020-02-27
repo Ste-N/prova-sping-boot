00c96b9e15cddadd12fca1a31606a7ae95be5020
Ste <56863742+Ste-N@users.noreply.github.com>
2020-02-27 17:09:35 +0100

    Add files via upload

---
+++ aereo5/src/main/java/com/aereo5/aereo5/Aereo5Application.java
@@ -0,0 +1,13 @@
+package com.aereo5.aereo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aereo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aereo5Application.class, args);
+	}
+
+}
---
+++ aereo5/src/test/java/com/aereo5/aereo5/Aereo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aereo5.aereo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aereo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
